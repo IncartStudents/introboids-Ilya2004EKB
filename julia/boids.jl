e9fc6e839441072058dfd250ea9807bd5e1e4133
Ilya2004EKB <ilusha.starikov@gmail.com>
2024-10-21 19:10:18 +0300

    21.10.2024

--- julia/boids.jl
+++ julia/boids.jl
@@ -1,19 +1,28 @@
 module Boids
+ import Pkg; Pkg.add("Plots")
 using Plots
 
 mutable struct WorldState
     boids::Vector{Tuple{Float64, Float64}}
+    vel_vector::Vector{Tuple{Float64, Float64}}
     height::Float64
     width::Float64
     function WorldState(n_boids, h, w)
         boids = [(rand(0:w), rand(0:h)) for _ in 1:n_boids]
-        new(boids, h, w)
+        for k in 1:n_boids
+            vel_module =rand(0:1) * 0.1
+            vel_angle = rand(-π:π)
+            vel_vector =  [vel_module * cos(vel_angle), vel_module * sin(vel_angle)]
+        end
+        new(boids, h, w, vel_vector)
     end
 end
 
-function update!(state::WorldState)
-    state.boids[1] = state.boids[1] .+ 0.1
-    # TODO: реализация алгоритма
+function update!(state::WorldState, n_boids)
+    for k in 1:n_boids
+        state.boids[k] = broadcast(+, state.boids[k], state.vel_vector[k])
+    end
+    # TODO: 
     return nothing
 end
 
@@ -25,7 +34,7 @@ function (@main)(ARGS)
     state = WorldState(n_boids, h, w)
 
     anim = @animate for time = 1:100
-        update!(state)
+        update!(state, n_boids)
         boids = state.boids
         scatter(boids, xlim = (0, state.width), ylim = (0, state.height))
     end
